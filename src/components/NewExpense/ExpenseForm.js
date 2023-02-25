b865843f594c488133dd5fb22229e86c4f71f21d
Rowoon Lee <dev.rowoon@gmail.com>
2023-02-25 16:23:59 +0900

    [학습] 여러개의 인풋요소 state 하나로 다루기

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,8 +1,34 @@
+import { useState } from "react";
 import "./ExpenseForm.css";
 
 export default function ExpenseForm() {
+  const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    enteredAmount: "",
+    enteredDate: "",
+  });
+
   const titleChangeHandler = (e) => {
-    console.log(e.target.value);
+    // setUserInput({
+    //   // state를 업데이트 하면 여러개의 객체를 한번에 다루는 경우 업데이트 되는 객체를 제외한 나머지를 잃어버리기 때문에 객체를 복사하여 나머지를 잃어버리지 않게 한다
+    //   ...userInput,
+    //   enteredTitle: e.target.vlaue,
+    // });
+    setUserInput((prevState) => {
+      return { ...prevState, enteredTitle: e.target.value };
+    });
+  };
+
+  const amountChangeHandler = (e) => {
+    setUserInput((prevState) => {
+      return { ...prevState, enteredAmount: e.target.value };
+    });
+  };
+
+  const dataChangeHandler = (e) => {
+    setUserInput((prevState) => {
+      return { ...prevState, enteredDate: e.target.value };
+    });
   };
 
   return (
@@ -14,11 +40,21 @@ export default function ExpenseForm() {
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dataChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">

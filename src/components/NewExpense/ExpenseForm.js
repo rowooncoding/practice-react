b874928e51f8177a0c587c375f58a09b1ac53321
Rowoon Lee <dev.rowoon@gmail.com>
2023-02-25 16:59:22 +0900

    [학습] 양방향 바인딩 학습 (인풋 제출을 하면 벨류가 빈문자열로 바뀜)

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -26,6 +26,10 @@ export default function ExpenseForm() {
       amount: enteredAmount,
       date: new Date(enteredDate),
     };
+    console.log(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   };
 
   return (
@@ -33,7 +37,11 @@ export default function ExpenseForm() {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -41,6 +49,7 @@ export default function ExpenseForm() {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -50,6 +59,7 @@ export default function ExpenseForm() {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            value={enteredDate}
             onChange={dataChangeHandler}
           />
         </div>

cfa3390a5c926054b291a9ff4137c27d70bb0f28
Rowoon Lee <dev.rowoon@gmail.com>
2023-02-25 15:28:48 +0900

    [추가] 폼 양식 입력

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -27,7 +28,7 @@ function App() {
   ];
   return (
     <div>
-      <h2>Let's Get Started</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );

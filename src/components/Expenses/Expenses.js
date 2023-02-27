888233135c84e6542c6b80501ec90e44f6f07782
Rowoon Lee <dev.rowoon@gmail.com>
2023-02-27 16:30:34 +0900

    [추가] 조건 명령문 반환

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import ExpensesList from "./ExpensesList";
 
 export default function Expenses(props) {
   const [filteredYear, setfilteredYear] = useState("2020");
@@ -13,17 +13,6 @@ export default function Expenses(props) {
     // data.getFullYear() : data에서 4자리수 연도를 가져온다
     return expense.date.getFullYear().toString() === filteredYear;
   });
-  let expensesContent = <p>No expenses found.</p>;
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
 
   return (
     <div>
@@ -32,7 +21,7 @@ export default function Expenses(props) {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {expensesContent}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
